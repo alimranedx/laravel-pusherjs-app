a1b9b48fd836f6d697ff2922f3419785c8d8e140
Al Imran Sagor <alimran.edx@gmail.com>
2025-11-06 00:03:45 +0600

    initial commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Events\PostCreate;
+
+class PostController extends Controller
+{
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function index(Request $request)
+    {
+        $posts = Post::get();
+
+        return view('posts', compact('posts'));
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $post = Post::create([
+            'user_id' => auth()->id(),
+            'title' => $request->title,
+            'body' => $request->body
+        ]);
+
+        event(new PostCreate($post));
+
+        return back()->with('success','Post created successfully.');
+    }
+    public function dummyPost(){
+        $post = Post::create([
+            'user_id' => auth()->id(),
+            'title' => "Dummy Post",
+            'body' => "This is a dummy post."
+        ]);
+
+        event(new PostCreate($post));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PostController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/dummy-posts-store', [PostController::class, 'dummyPost'])->name('dummy.posts.store');
